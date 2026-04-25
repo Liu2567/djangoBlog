a6e68e71c1d434f3a02ae2870b58b4c48aad696a
liu <207283221@qq.com>
2026-04-25 17:49:51 +0800

    访问限制的处理 @login_required装饰器实现登录验证 前端中，链接的访问控制逻辑

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -12,6 +13,9 @@ def blog_detail(request, blog_id):
     return render(request, 'blog/blog_detail.html')
 
 
-# 发布页面
+# 发布页面（需要登录）
+@login_required(login_url='blogAuth:login')
+# 还有一种方法，@login_required，然后在settings.py中配置LOGIN_URL = '/auth/login'
+# 如果页面需要登录后才能访问，也可以在前端中直接隐藏或禁用
 def blog_publish(request):
     return render(request, 'blog/blog_publish.html')
